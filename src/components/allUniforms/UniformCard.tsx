fc6e6a6266c0da9fab8466827c3f3f88b8f0f95d
Carlos Sosa <carlosupremedev@gmail.com>
2025-02-02 21:05:26 -0600

    fix: adding a image placeholder

--- src/components/allUniforms/UniformCard.tsx
+++ src/components/allUniforms/UniformCard.tsx
@@ -12,7 +12,7 @@ export default function UniformCard({ uniform }: { uniform: Uniform }) {
     return <div
         className="p-0 w-full flex flex-col flex-1 min-h-0 min-w-0 ">
         <Link to={uniformLink}>
-            <img src={IMAGES_URL + fotos[0].url} alt="" className="h-52 w-full object-top object-cover cursor-pointer" />
+            <img src={fotos && fotos.length > 0 ? IMAGES_URL + fotos[0].url : "/placeholder.png"} alt="" className="h-52 w-full object-top object-cover cursor-pointer" />
         </Link>
 
         <div className="flex flex-col text-sm gap-1 p-3 bg-white">
--- src/pages/AllUniforms.tsx
+++ src/pages/AllUniforms.tsx
@@ -4,29 +4,29 @@ import UniformCard from "../components/allUniforms/UniformCard.tsx";
 import { useGetUniforms } from "../hooks/useGetUniforms.ts";
 import { Uniform } from "../models/Uniform.ts";
 import FilterModal from "../components/ui/FilterModal.tsx";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function AllUniforms() {
-    const {data, isPending} = useGetUniforms();
+    const { data, isPending } = useGetUniforms();
     const [uniformsByCategory, setUniformsByCategory] = useState<Uniform[]>([]);
     const [uniforms, setUniforms] = useState<Uniform[]>([]);
-    const filterByCategory= (category: string)=>{
-        const isTodosCategory=category.length>0;
-        if(isTodosCategory) {
+    const filterByCategory = (category: string) => {
+        const isTodosCategory = category.length > 0;
+        if (isTodosCategory) {
             const filteredByCategory = data?.filter((uniform: Uniform) => uniform.categoria.toLowerCase() === category.toLowerCase());
             setUniforms(filteredByCategory ?? []);
             setUniformsByCategory(filteredByCategory ?? []);
         } else {
-            setUniforms(data?? []);
-            setUniformsByCategory(data??[]);
+            setUniforms(data ?? []);
+            setUniformsByCategory(data ?? []);
         }
     }
-    const filterByLeague=(league:string)=>{
-            const filteredByLeague=uniformsByCategory?.filter((uniform: Uniform) => uniform.liga?.nombre?.toLowerCase() === league.toLowerCase());
-            setUniforms(filteredByLeague ?? []);
+    const filterByLeague = (league: string) => {
+        const filteredByLeague = uniformsByCategory?.filter((uniform: Uniform) => uniform.liga?.nombre?.toLowerCase() === league.toLowerCase());
+        setUniforms(filteredByLeague ?? []);
     }
-    useEffect(()=>{
-        if(data) {
+    useEffect(() => {
+        if (data) {
             setUniforms(data);
             setUniformsByCategory(data);
         }
@@ -34,14 +34,14 @@ export default function AllUniforms() {
     return (
         <Layout loading={isPending}>
             <h1 className="bg-white top-0 left-0 z-10 font-bold text-xl p-5 sticky">Uniformes</h1>
-            <CategorySelector onSelect={filterByCategory}/>
-            <hr/>
+            <CategorySelector onSelect={filterByCategory} />
+            <hr />
             <div className="font-semibold p-5 flex items-center justify-between">
                 <p className="text-gray-500">{uniforms?.length} resultados</p>
-               <FilterModal onSelect={filterByLeague}/>
+                <FilterModal onSelect={filterByLeague} />
             </div>
             <div className="grid grid-cols-2 gap-x-2 w-full">
-                {uniforms?.map((uniform: Uniform) => (<UniformCard key={uniform.documentId} uniform={uniform}/>))}
+                {uniforms?.map((uniform: Uniform) => (<UniformCard key={uniform.documentId} uniform={uniform} />))}
             </div>
         </Layout>
     )
